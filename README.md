475a46a398df314b9610371196efb2fa5d61ec39
Evrard <axel91evrard@gmail.com>
2018-05-29 11:14:33 +0200

    Entity User

--- README.md
+++ README.md
@@ -24,3 +24,17 @@ php bin/console make:controller
 2) Highlights
 3) Partner
 4) Recrutement
+
+Modifier le .env avec le lien vers ma base
+
+Créer la bdd:
+
+php bin/console doctrine:database:create
+
+Créer mon entité User avec un Name et un Role
+
+php bin/console make:entity
+
+php bin/console make:migration
+
+Puis ajouter un email à ce même user avec les même commandes
---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Role;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->Role;
+    }
+
+    public function setRole(string $Role): self
+    {
+        $this->Role = $Role;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
